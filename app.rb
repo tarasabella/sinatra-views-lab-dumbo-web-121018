8e1dafae02577c021e7e6f2bcff4d0cd418aa325
Tara Sabella <tarasabella@gmail.com>
2019-01-07 21:59:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,13 +4,6 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-	require 'sinatra'
-class App < Sinatra::Base
-
-  get '/' do 
-    "Hello, World!"
-  end
-
 end
   get "/" do 
    "Hello, World!"
